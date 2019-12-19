a84672c52b59854672ba43c316a214b6fabc771f
sydberg <57270057+sydberg@users.noreply.github.com>
2019-12-18 18:49:59 -0600

    Data Parser

---
+++ dataparse.cc
@@ -0,0 +1,47 @@
+#include <fstream>
+#include <iostream>
+#include <cstdlib>
+#include <istream>
+
+using namespace std;
+using Strings = std::strings;
+using Check = std::check;
+
+
+int main(int argc, const char * argv[]) {
+	int i;
+
+
+	std::ifstream inputFileStream("output.csv");
+    	int count;
+    	
+    	for (i = 0; i < count; i++){
+    		String line;
+    		std::getline(inputFileStream, line);
+    		std::istringstream(inputFileStream, line); //because istringstream can look through lines better than fstream can making it better for parsing !!!!!!!!!
+    	}
+			outputid = Strings ID;
+			outputchoice = Strings ChoiceNum;
+			outputproj = Strings ProjID:
+
+    std::ifstream inputFile("pweek.db");
+    	for (i = 0; i < count; i++){
+    		String line;
+    		std::getline(inputFile, line);
+    		std::istringstream(inputFile, line);
+    	}
+
+    		inputid = Check ID;
+    		inputchoice = Check ChoiceNum;
+    		inputproj = Check ProjID;
+
+    while(iputid = outputid; inputchoice = outputchoice; inputproj = outputproj){
+    		line++;
+    		count++;
+    		return "mhm";
+    	}
+    cout << linestream(inputid != outpudid) << endl;
+    cout << linestream(inputchoice != outputchoice) << endl;
+    cout << linestream(inputproj != outputproj) << endl;
+
+}
